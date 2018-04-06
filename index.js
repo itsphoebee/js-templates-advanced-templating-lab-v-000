0e8f9c678021e2157d6f714eed2f28166ac61334
Phoebe Lau <phoebelau7@gmail.com>
2018-04-06 20:37:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function init() {
   })
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
 }
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
