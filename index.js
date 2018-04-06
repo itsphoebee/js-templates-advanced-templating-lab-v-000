2d6e2ae868f7d5d09f632903f9e213cb1563e335
Phoebe Lau <phoebelau7@gmail.com>
2018-04-06 20:01:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  function renderRecipe() {
+    var template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+    var html = template(getRecipe());
+  }
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function createRecipe() {
+  var recipe = getRecipe()
+  var recipeTemplate = document.getElementById("recipe-template").innerHTML
+  var template = Handlebars.compile(recipeTemplate)
+  document.getElementById("main").innerHTML = template(recipe)
+}
+
+function getRecipe() {
+  var ingredientsNodes = document.getElementsByName("ingredients")
+  var ingredients = []
+  for(var i=0;i<ingredientsNodes.length;i++) {
+    if(ingredientsNodes[i].value !== "") {
+      ingredients.push(ingredientsNodes[i].value)
+    }
+  }
+  var name = document.getElementById("name").value
+  var description = document.getElementById("description").value
+  var recipe = {name, ingredients, description}
+  return(recipe)
+}
