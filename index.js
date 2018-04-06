f4fad796dd597df9e95946986da17ae8d2414edc
Phoebe Lau <phoebelau7@gmail.com>
2018-04-06 20:28:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function init() {
   //put any page initialization/handlebars initialization here
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
-  Handlebars.registerHelper('displayIngredient', function(){
-
+  Handlebars.registerHelper('displayIngredient', function(ingredient){
+    return new Handlebars.SafeString('<li>' + ingredient + '</li>');
   })
 }
 document.addEventListener("DOMContentLoaded", function(event) {
